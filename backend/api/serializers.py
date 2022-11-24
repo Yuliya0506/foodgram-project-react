ce1d680b4bb4ee2f346114614cf737c6505be365
Yuliya0506 <gorcha79@mail.ru>
2022-11-24 21:44:26 +0400

    деплой

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,9 @@
-from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
 from django.db.models import F
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from rest_framework.response import Response
 
 from recipes.models import Ingredient, IngredientAmount, Recipe, Tag
 from users.models import Follow
