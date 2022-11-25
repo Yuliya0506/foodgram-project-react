a56eb4175386d79cd86a45a4b030623145192720
Yuliya0506 <gorcha79@mail.ru>
2022-11-25 23:51:35 +0400

    деплой

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -198,6 +198,9 @@ class FollowSerializer(serializers.ModelSerializer):
         return data
 
     def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
         return Follow.objects.filter(
             user=obj.user, author=obj.author
         ).exists()
