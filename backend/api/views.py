025a350ace0188e751d7f8a639fef1ec39a70596
Yuliya0506 <gorcha79@mail.ru>
2022-11-26 23:43:39 +0400

    деплой

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.db.models import BooleanField, Exists, OuterRef, Sum, Value
+from django.db.models import BooleanField, Exists, OuterRef, Value
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
