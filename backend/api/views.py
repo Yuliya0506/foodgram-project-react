ecfe7670790db3fcf2f5b35065c00ee9cc08164b
Yuliya0506 <gorcha79@mail.ru>
2022-11-26 22:28:08 +0400

    деплой

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.db.models import BooleanField, Exists, OuterRef, Value
+from django.db.models import BooleanField, Exists, OuterRef, Sum, Value
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import viewsets
@@ -10,7 +13,7 @@ from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 
 from recipes.models import (
-    Cart, Favorite, Ingredient,
+    Cart, Favorite, Ingredient, IngredientAmount,
     Recipe, Tag
 )
 from users.models import Follow
@@ -22,7 +25,6 @@ from .serializers import (
     RecipeReadSerializer, RecipeWriteSerializer, ShortRecipeSerializer,
     TagSerializer
 )
-from .services import generate_shop_cart
 
 User = get_user_model()
 
@@ -153,7 +155,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
             'errors': 'Ошибка удаления рецепта из списка'
         }, status=HTTPStatus.BAD_REQUEST)
 
-    @action(detail=False, permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        shop_list = generate_shop_cart(request.user)
-        return Response(shop_list, content_type='text/plain')
+        user = request.user
+        if not user.shopping_cart.exists():
+            return Response(status=HTTPStatus.BAD_REQUEST)
+
+        ingredients = IngredientAmount.objects.filter(
+            recipe__shopping_cart__user=request.user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).annotate(amount=Sum('amount'))
+
+        today = datetime.today()
+        shopping_list = (
+            f'Список покупок для: {user.get_full_name()}\n\n'
+            f'Дата: {today:%Y-%m-%d}\n\n'
+        )
+        shopping_list += '\n'.join([
+            f'- {ingredient["ingredient__name"]} '
+            f'({ingredient["ingredient__measurement_unit"]})'
+            f' - {ingredient["amount"]}'
+            for ingredient in ingredients
+        ])
+        shopping_list += f'\n\nFoodgram ({today:%Y})'
+
+        filename = f'{user.username}_shopping_list.txt'
+        response = HttpResponse(shopping_list, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename={filename}'
+
+        return response
