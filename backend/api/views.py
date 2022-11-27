7284eb2564b053806638f2fadfaba6337890a5b5
Yuliya0506 <gorcha79@mail.ru>
2022-11-27 10:49:09 +0400

    деплой

--- backend/api/views.py
+++ backend/api/views.py
@@ -24,7 +24,6 @@ from .serializers import (
     RecipeReadSerializer, RecipeWriteSerializer, ShortRecipeSerializer,
     TagSerializer
 )
-from .services import generate_shop_cart
 
 User = get_user_model()
 
@@ -162,7 +161,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
             recipe__cart__user=request.user).values(
             'ingredients__name',
             'ingredients__measurement_unit').annotate(total=Sum('amount'))
-        shopping_cart = generate_shop_cart(ingredients)
+
+        shopping_cart = '\n'.join([
+            f'{ingredient["ingredients__name"]} - {ingredient["total"]} '
+            f'{ingredient["ingredients__measurement_unit"]}'
+            for ingredient in ingredients
+        ])
         response = HttpResponse(shopping_cart, content_type='text/plain')
-        response['Content-Disposition'] = f'attachment; {settings.FILENAME}'
+        response['Content-Disposition'] = f'attachment; filename={settings.FILENAME}'
         return response
